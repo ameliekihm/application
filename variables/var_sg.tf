bed1d665de5d305914f4f3e33f51aa9702a986f1
Amelie Kihm <ameliekihm@gmail.com>
2025-07-22 15:58:01 -0600

    Add SG variables for slamd_useast1

---
+++ variables/var_sg.tf
@@ -0,0 +1,109 @@
+variable "sg_variables" {
+  default = {
+
+    ec2 = {
+      tags = {
+
+        neopindapne2 = {
+          Name    = "hello-neopind_apnortheast2-ec2-sg"
+          app     = "hello"
+          project = "hello"
+          env     = "dev"
+          stack   = "neopind_apnortheast2"
+        },
+
+        devartdapne2 = {
+          Name    = "hello-devartd_apnortheast2-ec2-sg"
+          app     = "hello"
+          project = "hello"
+          env     = "dev"
+          stack   = "devartd_apnortheast2"
+        },
+
+        devartsapne2 = {
+          Name    = "hello-devarts_apnortheast2-ec2-sg"
+          app     = "hello"
+          project = "hello"
+          env     = "dev"
+          stack   = "devarts_apnortheast2"
+        },
+
+        artdapne2 = {
+          Name    = "hello-devartd_apnortheast2-ec2-sg"
+          app     = "hello"
+          project = "hello"
+          env     = "dev"
+          stack   = "devartd_apnortheast2"
+        },
+
+        artpapne2 = {
+          Name    = "hello-artp_apnortheast2-ec2-sg"
+          app     = "hello"
+          project = "hello"
+          env     = "prod"
+          stack   = "artp_apnortheast2"
+        },
+
+        slamd_useast1 = {
+          Name    = "webapp-slamd_useast1-ec2-sg"
+          app     = "webapp"
+          project = "webapp"
+          env     = "dev"
+          stack   = "slamd_useast1"
+        }
+      }
+    }
+
+    external_lb = {
+      tags = {
+
+        neopindapne2 = {
+          Name    = "hello-neopind_apnortheast2-external-lb-sg"
+          app     = "hello"
+          project = "hello"
+          env     = "dev"
+          stack   = "neopind_apnortheast2"
+        },
+
+        devartdapne2 = {
+          Name    = "hello-devartd_apnortheast2-external-lb-sg"
+          app     = "hello"
+          project = "hello"
+          env     = "dev"
+          stack   = "devartd_apnortheast2"
+        },
+
+        devartsapne2 = {
+          Name    = "hello-devarts_apnortheast2-external-lb-sg"
+          app     = "hello"
+          project = "hello"
+          env     = "dev"
+          stack   = "devarts_apnortheast2"
+        },
+
+        artdapne2 = {
+          Name    = "hello-artd_apnortheast2-external-lb-sg"
+          app     = "hello"
+          project = "hello"
+          env     = "dev"
+          stack   = "artd_apnortheast2"
+        },
+        artpapne2 = {
+          Name    = "hello-artp_apnortheast2-external-lb-sg"
+          app     = "hello"
+          project = "hello"
+          env     = "prod"
+          stack   = "artp_apnortheast2"
+        },
+        
+        slamd_useast1 = {
+          Name    = "webapp-slamd_useast1-external-lb-sg"
+          app     = "webapp"
+          project = "webapp"
+          env     = "dev"
+          stack   = "slamd_useast1"
+        }
+      }
+    }
+  }
+}
