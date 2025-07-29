165ff65610133670a5e8f89c7aa2e0c0429d5ea2
Amelie Kihm <ameliekihm@gmail.com>
2025-07-28 21:32:49 -0600

    fix: update Route53 variables in slamd_useast1

--- slamd_useast1/var_route53.tf
+++ slamd_useast1/var_route53.tf
@@ -1,7 +1,7 @@
 variable "r53_variables" {
   default = {
     id = {
-      slamd_webapp_com_zone_id         = "Z083585316MUCG67ZX4Q"
+      slamd_webapp_com_zone_id         = "Z099164143YGLJYOPJFK"
       slamd_webapp_com_acm_arn_useast1 = "arn:aws:acm:us-east-1:835175800383:certificate/2ba43c88-f828-4db6-8c7b-e89dc3439e75"
     }
   }
