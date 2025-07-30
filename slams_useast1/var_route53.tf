919e5feaac7c273e650b69a5dfd1b32b0a57ac2e
Amelie Kihm <ameliekihm@gmail.com>
2025-07-29 20:47:57 -0600

    Add Terraform code for slams VPC

---
+++ slams_useast1/var_route53.tf
@@ -0,0 +1,9 @@
+variable "r53_variables" {
+  default = {
+    id = {
+      slamd_webapp_com_zone_id         = "Z099164143YGLJYOPJFK"
+      slamd_webapp_com_acm_arn_useast1 = "arn:aws:acm:us-east-1:835175800383:certificate/2ba43c88-f828-4db6-8c7b-e89dc3439e75"
+    }
+  }
+}
+
