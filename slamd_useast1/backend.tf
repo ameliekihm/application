1665385834747be5814f1df88681a76e6bbc2b3c
Amelie Kihm <ameliekihm@gmail.com>
2025-07-31 22:36:24 -0600

    chore: update backend S3 key path from application to webapp

--- slamd_useast1/backend.tf
+++ slamd_useast1/backend.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     bucket         = "tf101-amelie-use1-tfstate"
-    key            = "terraform/services/application/slamd_useast1/terraform.tfstate"
+    key            = "terraform/services/webapp/slamd_useast1/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
